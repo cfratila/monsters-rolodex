1261f7f999fbfd5cbba4bb418175627d9119c5e4
Carmen Fratila <pop.carmen@gmail.com>
2019-08-09 16:30:04 +0300

    Updates

---
+++ precache-manifest.ee2e43c9def64d9c2e99122c0200ea2e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da04a32640fe5eb66a3ccedaad78ccf6",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "cd101ac556956e53b1e4",
+    "url": "/monsters-rolodex/static/css/main.0f297afd.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "cd101ac556956e53b1e4",
+    "url": "/monsters-rolodex/static/js/main.7b0456eb.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
